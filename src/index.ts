ae5b80afebec2b86f3d73c04fe5c2ac3d0597c41
William Francis <william@gradconnection.com>
2018-03-15 14:06:21 +0200

    Added better response

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import {SlackWebhook} from "./integrations/slack-webhook";
-import {environment} from "./environment";
-import {incomingMessage, slashCommand} from "./services/slack";
-import {start} from "./web-server/http-server";
-import {SlackMentionPayload} from "./models/slack-mention-payload";
-import {SlackSlashPayload} from "./models/slack-slash-payload";
-import {Root} from "./integrations/root";
+import { SlackWebhook } from "./integrations/slack-webhook";
+import { environment } from "./environment";
+import { incomingMessage, slashCommand } from "./services/slack";
+import { start } from "./web-server/http-server";
+import { SlackMentionPayload } from "./models/slack-mention-payload";
+import { SlackSlashPayload } from "./models/slack-slash-payload";
+import { Root } from "./integrations/root";
 
 const whURL = environment.INCOMING_WEBHOOK_URL;
 const RootURL = environment.ROOT_API_URL;
@@ -15,47 +15,60 @@ let response = [];
 export let bottie = new SlackWebhook(whURL);
 
 // Example of event fired when slash command is used
-slashCommand.subscribe((payload:SlackSlashPayload) => {
-  console.log("payload",payload);
-  bottie.send('BOT RESPONSE', payload.response_url);
+slashCommand.subscribe((payload: SlackSlashPayload) => {
+  console.log("payload", payload);
+  bottie.send("BOT RESPONSE", payload.response_url);
 });
 
 // Example of event fired when app is mentioned
-incomingMessage.subscribe((payload:SlackMentionPayload) => {
-  console.log("payload inside incomingMessage",payload);
-  if(!response[payload.event.user])
-  {
-    response[payload.event.user] =[];
+incomingMessage.subscribe((payload: SlackMentionPayload) => {
+  console.log("payload inside incomingMessage", payload);
+  if (!response[payload.event.user]) {
+    response[payload.event.user] = [];
   }
   step++;
-  response[payload.event.user][step-1]=(payload.event.text.replace('<@U9Q73AKKM>',''));
-  console.log("step",step);
-  console.log("response",response);
-  
-  switch(step){
+  response[payload.event.user][step - 1] = payload.event.text.replace(
+    "<@U9Q73AKKM>",
+    ""
+  );
+  console.log("step", step);
+  console.log("response", response);
+
+  switch (step) {
     case 1:
-    bottie.send('Hi there, what is your name? '  )
-    break;
+      bottie.send("Hi there, what is your name? ");
+      break;
 
     case 2:
-    bottie.send('Hi' + response[payload.event.user][step-1] + ' what is your number? '  )
-    break;
+      bottie.send(
+        "Hi" + response[payload.event.user][step - 1] + " what is your number? "
+      );
+      break;
 
     case 3:
-    bottie.send('What type of phone do you have? that has the phone number ' + response[payload.event.user][step-1])
-    break;
+      bottie.send(
+        response[payload.event.user][step - 1] + " ? What type of phone is it?"
+      );
+      break;
 
     case 4:
-    bottie.send(response[payload.event.user][step-1] + " that's a cool phone")
-    break;
+      if (response[payload.event.user][step - 1] == " Nokia") {
+        bottie.send(
+          response[payload.event.user][step - 1] + "? Lol, Do they still make those?"
+        );
+      } else {
+        bottie.send(
+          response[payload.event.user][step - 1] + " that is a cool phone"
+        );
+      }
+      break;
 
     default:
-    bottie.send('Hi there'  )   
-    break;
-
-  }  
-  if(step>3){
-    step=0;
+      bottie.send("Hi there");
+      break;
+  }
+  if (step > 3) {
+    step = 0;
   }
   //bottie.send('Hi there, what do you need?')
 });
@@ -63,11 +76,13 @@ incomingMessage.subscribe((payload:SlackMentionPayload) => {
 let root = new Root(RootURL, RootToken);
 
 // EXAMPLES
-root.post("v1/insurance/quotes", {type: "root_gadgets", model_name: "iPhone 5"}).then((res) => {
-  console.log(res);
-});
+root
+  .post("v1/insurance/quotes", { type: "root_gadgets", model_name: "iPhone 5" })
+  .then(res => {
+    console.log(res);
+  });
 
-root.get('v1/insurance/modules/root_gadgets/models').then((res) => {
+root.get("v1/insurance/modules/root_gadgets/models").then(res => {
   console.log(res);
 });
 
