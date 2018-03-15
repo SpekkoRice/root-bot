006899f207404c7f9484b69506e8bd792786ab5e
Zander <zander@evalgeniuses.com>
2018-03-15 11:23:50 +0200

    adding boilerplate for duplicate payloads

--- src/web-server/routes.ts
+++ src/web-server/routes.ts
@@ -1,12 +1,24 @@
 import * as Router from "koa-router";
 import {incomingMessage, slashCommand} from "../services/slack";
+import * as _ from "lodash";
 
 export let router = new Router();
 
 router.get("/", async (ctx:any, next:any) => { ctx.body = "HELLO WORLD!" });
 
+
+let tokens = [];
+
 router.post("/bottie", async (ctx:any, next:any) => {
-  incomingMessage.next(ctx.request.body);
+  let token = ctx.request.body.token;
+  let inArray = _.findIndex(tokens, (i) => {
+    return i == token;
+  });
+
+  if(!inArray) {
+    incomingMessage.next(ctx.request.body);
+    tokens.push(token);
+  }
 });
 
 router.post('/slash-bottie', async (ctx:any, next:any) => {
